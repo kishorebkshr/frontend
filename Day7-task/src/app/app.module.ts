928321e6fbc795dced7603a0c4036f6260c7078a
kishorebkshr <85120694+kishorebkshr@users.noreply.github.com>
2021-06-07 21:01:12 +0530

    Add files via upload
    Day7-task

---
+++ Day7-task/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BaseComponent } from './base/base.component';
+import { LoginComponent } from './base/login/login.component';
+import { RegisterComponent } from './base/register/register.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    BaseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
